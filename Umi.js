5c919227b4e4c6311f7347ee4553382596bf04bf
dog-industry <91227995+dog-industry@users.noreply.github.com>
2022-02-27 12:01:20 -0600

    alhpa1

---
+++ Umi.js
@@ -0,0 +1,9 @@
+nombrejugador1=localStorage.getItem("nombrejugador1");
+nombrejugador2=localStorage.getItem("nombrejugador2");
+puntajej1=0;
+puntajej2=0;
+
+document.getElementById("nombrejugador1").innerHTML=nombrejugador1+" : ";
+document.getElementById("nombrejugador2").innerHTML=nombrejugador2+" : ";
+document.getElementById("puntajej1").innerHTML=puntajej1;
+document.getElementById("puntajej2").innerHTML=puntajej2;
